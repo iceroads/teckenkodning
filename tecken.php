3ec8f64ec84308a45f03b0e5f30748dace7a2cb0
Pontus Nilsson <pontus@nilfam.se>
2019-11-05 12:28:51 +0100

    added a while loop to display database inserts

--- tecken.php
+++ tecken.php
@@ -15,23 +15,19 @@ try {
      * I med vår databas är i latin1_swedish_ci vill vi använda latin1
      * i detta fallet.
      */
-    //$pdo->exec("SET NAMES latin1");
+    $pdo->exec("SET NAMES latin1");
 
-    /*
-     * Genom att tvinga anslutningen till att läsa och skriva i UTF8 kan vi
-     * spara text som UTF8 även om databasen är latin1
-     */
-
-    $pdo->exec("SET NAMES utf8");
 } catch (PDOException $e) {
     // Skriv ut felmeddelandet vi får ifall något har gått fel
     echo $e->getMessage();
     die();
 }
+// Hämta all data från tabellen users
+$stmt = $pdo->query("SELECT * FROM `users`");
 
-$user_bio = 'Jag gillar teckenkodning och är sjukt 
-             intresserad av allt med ÅÄÖ och andra konstiga tecken
-            ';
-
-// Skicka in ovanstående text till databasen.
-$pdo->query("INSERT INTO `users`(bio) VALUES('{$user_bio}')");
+/*
+ * För varje rad i tabellen, hämta som ett object
+ */
+while ($row = $stmt->fetchObject()) {
+    echo $row->bio . "<br>";
+}
